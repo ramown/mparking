ec0e40fb260ecad950616d4d791f0b7d733454f5
ramown <emmanuelrlima@gmail.com>
2020-05-17 15:56:27 -0300

    ...

---
+++ app/controllers.py
@@ -0,0 +1,15 @@
+from app.models import *
+from django.http import HttpResponse
+
+
+# Create your views here.
+class AreaController():
+	def listar_areas():
+		return Area.objects.all()
+
+	def salvar_area(area_capturada):
+		area = Area()
+		area.nome = area_capturada['nome']
+		area.capacidade = area_capturada['capacidade']
+		area.save()
+    
