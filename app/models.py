35d7d4974ec587ad8f91eac327a9f78e5833d686
ramown <emmanuelrlima@gmail.com>
2020-05-17 15:49:17 -0300

    ...

--- app/models.py
+++ app/models.py
@@ -31,8 +31,9 @@ class Pessoa(models.Model):
 class Funcionario(Pessoa):
 	matricula = models.CharField(max_length=15)
 	usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    def __str__(self):
-    	return self.nome
+	
+	def __str__(self):
+		return self.nome
 
 class Proprietario(Pessoa):
 	TIPOS_PROPS = (
@@ -41,8 +42,9 @@ class Proprietario(Pessoa):
 		(2, 'ADMINISTRATIVO'), 
 		(3, 'VISITANTES'),) 
 	tipo = models.IntegerField(choices=TIPOS_PROPS)
-    def __str__(self):
-    	return self.nome
+	
+	def __str__(self):
+		return self.nome
 
 class Ocorrencia(models.Model):
 	TIPOS_OCORRENCIAS = (
@@ -77,8 +79,9 @@ class Veiculo(models.Model):
 	tipo = models.IntegerField(choices=CATEGORIAS_TIPO)
 	categoria = models.IntegerField(choices=CATEGORIAS_VEICULOS)
 	proprietario = models.ForeignKey(Proprietario, on_delete=models.CASCADE)
-    def __str__(self):
-    	return self.nome
+
+	def __str__(self):
+		return self.placa
 
 
 class Vaga(models.Model):
@@ -94,6 +97,11 @@ class Vaga(models.Model):
 		self.ocupada = False
 		self.save()
 
+	def __str__(self):
+		return "Vaga Livre []" if ocupada else "Vaga Ocupada"
+
+
+
 class LogDeEstacionamento(models.Model):
 	veiculo = models.ForeignKey(Veiculo, models.SET_NULL, blank=True, null=True)
 	area = models.ForeignKey(Area, models.SET_NULL, blank=True, null=True)
@@ -107,3 +115,6 @@ class LogDeEstacionamento(models.Model):
 	def saindo(self):
 		self.data_saida = timezone.now()
 		self.save()	
+
+	def __str__(self):
+		return "veiculo: {} -- {} :: entrada: {} /saida: {}".format(self.nome,self.area, self.data_entrada, self.data_saida)
