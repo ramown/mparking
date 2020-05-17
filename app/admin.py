2173d079f451e1c8ce5fb847ed33ee4d7f60f756
ramown <emmanuelrlima@gmail.com>
2020-05-17 12:23:21 -0300

    Criação dos modelos Veiculo e Vaga

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from app.models import Area, Evento
+from app.models import Area, Evento, Proprietario, Funcionario, Ocorrencia, Veiculo, Vaga
 
 
 # Register your models here.
 admin.site.register(Area)
 admin.site.register(Evento)
+admin.site.register(Proprietario)
+admin.site.register(Funcionario)
+admin.site.register(Ocorrencia)
+admin.site.register(Veiculo)
+admin.site.register(Vaga)
--- app/models.py
+++ app/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-'''
-'''
 class Area(models.Model):
     nome = models.CharField(max_length=30)
     capacidade = models.IntegerField()
@@ -16,6 +14,7 @@ class Evento(models.Model):
     data_final = models.DateField(null=True, blank=True)
     areas_usadas = models.ManyToManyField(Area)
 
+
 class Pessoa(models.Model):
 	nome = models.CharField(max_length=50)
 
@@ -24,6 +23,7 @@ class Funcionario(Pessoa):
 	matricula = models.CharField(max_length=15)
 	usuario = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
 class Proprietario(Pessoa):
 	TIPOS_PROPS = (
 		(0, 'ALUNO'), 
@@ -31,6 +31,7 @@ class Proprietario(Pessoa):
 		(2, 'ADMINISTRATIVO'), 
 		(3, 'VISITANTES'),) 
 	tipo = models.IntegerField(choices=TIPOS_PROPS)
+
 
 class Ocorrencia(models.Model):
 	TIPOS_OCORRENCIAS = (
@@ -43,4 +44,39 @@ class Ocorrencia(models.Model):
 	tipo = models.IntegerField(choices=TIPOS_OCORRENCIAS)
 	data_ocorrencia = models.DateField(null=True, blank=True)
 	data_registro = models.DateField(auto_now=True)
-	funcionario = models.ForeignKey(Funcionario, models.SET_NULL, blank=True, null=True,)
+	funcionario = models.ForeignKey(Funcionario, models.SET_NULL, blank=True, null=True)
+	descricao = models.TextField()
+
+
+class Veiculo(models.Model):
+	CATEGORIAS_VEICULOS = (
+		(0, 'PREFERENCIAL'),
+		(1, 'FUNCIONARIO'), 
+		(2, 'ALUNO'),
+ 		(6, 'VISITANTE'))
+	CATEGORIAS_TIPO = (
+		(0, 'CARRO'), 
+		(1, 'MOTOCICLETA'), 
+		(2, 'VAN'), 
+		(3, 'ÔNIBUS'),
+		)
+	placa = models.CharField(max_length=15)
+	modelo = models.CharField(max_length=20)
+	marca = models.CharField(max_length=15)
+	tipo = models.IntegerField(choices=CATEGORIAS_TIPO)
+	categoria = models.IntegerField(choices=CATEGORIAS_VEICULOS)
+	proprietario = models.ForeignKey(Proprietario, on_delete=models.CASCADE)
+        
+class Vaga(models.Model):
+	veiculo = models.ForeignKey(Veiculo, models.SET_NULL, blank=True, null=True)
+	area = models.ForeignKey(Area, models.SET_NULL, blank=True, null=True)
+	data_entrada = models.DateField(null=True, blank=True)
+	data_saida = models.DateField(null=True, blank=True)
+	
+	def entrando(self):
+		self.data_entrada = timezone.now()
+		self.save()
+
+	def saindo(self):
+		self.data_saida = timezone.now()
+		self.save()
